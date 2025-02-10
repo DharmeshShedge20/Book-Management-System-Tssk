ee372131b76bfbc222587798571964af308badaf
DharmeshShedge20 <161012079+DharmeshShedge20@users.noreply.github.com>
2025-02-10 16:09:04 +0530

    Add files via upload

---
+++ Frontend/src/pages/EditBook.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import BookForm from '../components/Form/BookForm'
+
+const EditBook = () => {
+  return (
+    <BookForm type={"Edit"}/>
+
+  )
+}
+
+export default EditBook
